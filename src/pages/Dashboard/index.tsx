02c4453185e8e95d8b5fc93d7015a379a83190ab
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2020-12-12 20:47:48 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" type="text" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/31122067?s=460&u=317a5b068fec5548665b44e4a9d00138d2d737df&v=4"
+            alt="Senhor Biscoito"
+          />
+          <div>
+            <strong>Jean Miranda</strong>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et,
+              magnam?
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/31122067?s=460&u=317a5b068fec5548665b44e4a9d00138d2d737df&v=4"
+            alt="Senhor Biscoito"
+          />
+          <div>
+            <strong>Jean Miranda</strong>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et,
+              magnam?
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/31122067?s=460&u=317a5b068fec5548665b44e4a9d00138d2d737df&v=4"
+            alt="Senhor Biscoito"
+          />
+          <div>
+            <strong>Jean Miranda</strong>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et,
+              magnam?
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
